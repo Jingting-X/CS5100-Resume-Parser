d87169957a1628305a202816bfac8e6d7e457428
jierenliu <liu.jier@northeastern.edu>
2022-11-15 19:06:20 -0800

    Add Basic Structure for Parser

---
+++ front_end/app.js
@@ -0,0 +1,22 @@
+function uploadFile(form) {
+    const formData = new FormData(form);
+    var oOutput = document.getElementById("static_file_response")
+    var resume = document.getElementById("resume_parser_table")
+    var oReq = new XMLHttpRequest();
+    oReq.open("POST", "upload_static_file", true);
+    oReq.onload = function(oEvent) {
+        if (oReq.status == 200) {
+            oOutput.innerHTML = "Uploaded!";
+            var results = oReq.response
+            results = JSON.parse(results)
+            resume_parser_table.innerHTML = results.name
+
+            console.log(results)
+        } else {
+            oOutput.innerHTML = "Error occurred when trying to upload your file.<br \/>";
+        }
+    };
+    oOutput.innerHTML = "Sending file!";
+    console.log("Sending file!")
+    oReq.send(formData);
+}
