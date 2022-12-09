620f14891ec064a734a5edfbb69e154c84240218
jierenliu <liu.jier@northeastern.edu>
2022-12-08 21:44:46 -0800

    add alert and change some words

--- front_end/app.js
+++ front_end/app.js
@@ -1,5 +1,11 @@
 function uploadFile(form) {
     const formData = new FormData(form);
+
+    if (1, document.getElementById("file").files.length == 0) {
+        alert("Please choose a file");
+        return;
+    }
+
     var oOutput = document.getElementById("static_file_response")
     var resume = document.getElementById("resume_parser_table")
     var oReq = new XMLHttpRequest();
